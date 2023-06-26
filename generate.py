8371de9ad3703d80404496252b77728f4fb18497
Peter Thoman <peter@ph3.at>
2023-06-27 00:02:49 +0200

    New data format, checkpoint updates, no longer keep all images in repo

--- generate.py
+++ generate.py
@@ -9,6 +9,11 @@ from subprocess import Popen
 
 output_dir = "C:/dev/ai/ComfyUI/output"
 
+starting_seed = 42
+imgs_per_prompt = 6
+# the number per batch has a huge performance impact, should probably be tuned for each target HW
+imgs_per_batch = 2
+
 with open("highres_fix.json", 'r') as file:
     prompt_text = file.read()
 
@@ -17,7 +22,7 @@ def queue_prompt(prompt):
     data = json.dumps(p).encode('utf-8')
     req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
     handler = request.urlopen(req)
-    print(handler.status)
+    print(handler.status, end=' ', flush=True)
 
 json_prompt = json.loads(prompt_text)
 
@@ -39,6 +44,14 @@ def set_checkpoint(ckpt):
 def set_output_prefix(prefix):
     json_prompt["26"]["inputs"]["filename_prefix"] = prefix
 
+def set_batch_size(sz):
+    json_prompt["5"]["inputs"]["batch_size"] = sz
+
+def set_seed(seed):
+    json_prompt["3"]["inputs"]["seed"] = seed
+    json_prompt["22"]["inputs"]["seed"] = seed
+
+
 # set_pos_prompt("armored knight")
 # set_output_prefix("absolutereality_v10")
 # set_checkpoint("absolutereality_v10.safetensors")
@@ -57,11 +70,14 @@ prompts = [
     ("running horse", 'horse'),
     ("snowy mountaintop", 'mountain'),
     ("bowl of salad", 'salad'),
+    ("couple in living room", 'couple'),
 ]
 checkpoints = [
     ('absolutereality_v10'             , 'absrealv10'    , 'photo', 'AbsoluteReality'                                     , 'https://civitai.com/models/81458?modelVersionId=86437'),
+    ('analogMadness_v50'               , 'analogmadv50'  , 'photo', 'Analog Madness - Realistic model v5.0'               , 'https://civitai.com/models/8030?modelVersionId=101080'),
     ('architecturerealmix_v10'         , 'archrealv10'   , 'photo', 'ArchitectureRealMix'                                 , 'https://civitai.com/models/84958/architecturerealmix'),
     ('aZovyaPhotoreal_v1Ultra'         , 'azovyaphotov1u', 'photo', 'A-Zovya Photoreal V1 Ultra'                          , 'https://civitai.com/models/57319?modelVersionId=61735'),
+    ('aZovyaPhotoreal_v2'              , 'azovyaphotov2' , 'photo', 'A-Zovya Photoreal V2'                                , 'https://civitai.com/models/57319?modelVersionId=99805'),
     ('cosplaymix_v20'                  , 'cosplayv20'    , 'photo', 'CosplayMix v2.0'                                     , 'https://civitai.com/models/34502?modelVersionId=48334'),
     ('cosplaymix_v41'                  , 'cosplayv41'    , 'photo', 'CosplayMix v4.1'                                     , 'https://civitai.com/models/34502?modelVersionId=98501'),
     ('cyberrealistic_v30'              , 'cyberrealv30'  , 'photo', 'CyberRealistic v3.0'                                 , 'https://civitai.com/models/15003?modelVersionId=79754'),
@@ -76,23 +92,26 @@ checkpoints = [
     ('rundiffusionFX_v10'              , 'rundifffxv10'  , 'photo', 'RunDiffusion FX Photorealistic'                      , 'https://civitai.com/models/82972/rundiffusion-fx-photorealistic'),
     ('urpmv13'                         , 'urpmv13'       , 'photo', 'Uber Realistic Porn Merge v1.3'                      , 'https://civitai.com/models/2661?modelVersionId=15640'),
 
-    ('aZovyaRPGArtistTools_v3VAE'  , 'azovyarpgv3' , 'other', 'A-Zovya RPG Artist Tools v3', 'https://civitai.com/models/8124?modelVersionId=87886'),
-    ('beenyou_r11'                 , 'beenyour11'  , 'other', 'BeenYou R11'                , 'https://civitai.com/models/27688?modelVersionId=64889'),
-    ('ddosmix_V2'                  , 'ddosmixv2'   , 'other', 'DDosMix v2'                 , 'https://civitai.com/models/8437?modelVersionId=12183'),
-    ('deliberate_v2'               , 'deliberatev2', 'other', 'Deliberate v2'              , 'https://civitai.com/models/4823?modelVersionId=15236'),
-    ('dreamshaper_4BakedVaeFp16'   , 'dreamshaper4', 'other', 'DreamShaper v4'             , 'https://civitai.com/models/4384?modelVersionId=24365'),
-    ('dreamshaper_6BakedVae'       , 'dreamshaper6', 'other', 'DreamShaper v6'             , 'https://civitai.com/models/4384?modelVersionId=78164'),
-    ('ghostmix_v20Bakedvae'        , 'ghostmixv20' , 'other', 'GhostMix v2.0'              , 'https://civitai.com/models/36520?modelVersionId=76907'),
-    ('lyriel_v16'                  , 'lyrielv16'   , 'other', 'Lyriel v1.6'                , 'https://civitai.com/models/22922?modelVersionId=72396'),
-    ('mdjrny-v4'                   , 'mdjrnyv4'    , 'other', 'OpenJourney v4'             , 'https://huggingface.co/prompthero/openjourney/blob/main/mdjrny-v4.safetensors'),
-    ('neverendingDreamNED_bakedVae', 'ned'         , 'other', 'NeverEnding Dream v1'       , 'https://civitai.com/models/10028?modelVersionId=11925'),
-    ('protogenV22Anime_22'         , 'protogenv22a', 'other', 'Protogen v2.2 (Anime)'      , 'https://civitai.com/models/3627/protogen-v22-anime-official-release'),
-    ('revAnimated_v121'            , 'revanimv121' , 'other', 'ReV Animated v1.2.1'        , 'https://civitai.com/models/7371?modelVersionId=40248'),
-    ('rpg_V4'                      , 'rpgv4'       , 'other', 'RPG v4'                     , 'https://civitai.com/models/1116?modelVersionId=7133'),
+    ('aZovyaRPGArtistTools_v3VAE'  , 'azovyarpgv3' , 'other', 'A-Zovya RPG Artist Tools v3'  , 'https://civitai.com/models/8124?modelVersionId=87886'),
+    ('beenyou_r11'                 , 'beenyour11'  , 'other', 'BeenYou R11'                  , 'https://civitai.com/models/27688?modelVersionId=64889'),
+    ('colorful_v31'                , 'colorfulv31' , 'other', 'Colorful v3.1'                , 'https://civitai.com/models/7279?modelVersionId=90599'),
+    ('ddosmix_V2'                  , 'ddosmixv2'   , 'other', 'DDosMix v2'                   , 'https://civitai.com/models/8437?modelVersionId=12183'),
+    ('deliberate_v2'               , 'deliberatev2', 'other', 'Deliberate v2'                , 'https://civitai.com/models/4823?modelVersionId=15236'),
+    ('dreamshaper_4BakedVaeFp16'   , 'dreamshaper4', 'other', 'DreamShaper v4'               , 'https://civitai.com/models/4384?modelVersionId=24365'),
+    ('dreamshaper_6BakedVae'       , 'dreamshaper6', 'other', 'DreamShaper v6'               , 'https://civitai.com/models/4384?modelVersionId=78164'),
+    ('ghostmix_v20Bakedvae'        , 'ghostmixv20' , 'other', 'GhostMix v2.0'                , 'https://civitai.com/models/36520?modelVersionId=76907'),
+    ('gameIconInstitute_v30'       , 'gameicov30'  , 'other', 'Game Icon Institute_mode v3.0', 'https://civitai.com/models/47800?modelVersionId=76533'),
+    ('lyriel_v16'                  , 'lyrielv16'   , 'other', 'Lyriel v1.6'                  , 'https://civitai.com/models/22922?modelVersionId=72396'),
+    ('mdjrny-v4'                   , 'mdjrnyv4'    , 'other', 'OpenJourney v4'               , 'https://huggingface.co/prompthero/openjourney/blob/main/mdjrny-v4.safetensors'),
+    ('neverendingDreamNED_bakedVae', 'ned'         , 'other', 'NeverEnding Dream v1'         , 'https://civitai.com/models/10028?modelVersionId=11925'),
+    ('protogenV22Anime_22'         , 'protogenv22a', 'other', 'Protogen v2.2 (Anime)'        , 'https://civitai.com/models/3627/protogen-v22-anime-official-release'),
+    ('revAnimated_v121'            , 'revanimv121' , 'other', 'ReV Animated v1.2.1'          , 'https://civitai.com/models/7371?modelVersionId=40248'),
+    ('rpg_V4'                      , 'rpgv4'       , 'other', 'RPG v4'                       , 'https://civitai.com/models/1116?modelVersionId=7133'),
 
     ('ambientgrapemix_v10'                   , 'ambgrapev10'     , 'anime', 'AmbientGrapeMix v1.0'   , 'https://civitai.com/models/30671?modelVersionId=37023'),
     ('anyloraCheckpoint_bakedvaeFtmseFp16NOT', 'anylora'         , 'anime', 'AnyLoRA - Checkpoint'   , 'https://civitai.com/models/23900?modelVersionId=29792'),
     ('anythingV5PrtRE'                       , 'anythingv5'      , 'anime', 'Anything v5'            , 'https://civitai.com/models/9409?modelVersionId=30163'),
+    ('AnythingV5Ink_v32Ink'                  , 'anythingv32ink'  , 'anime', 'Anything v3.2 [ink]'    , 'https://civitai.com/models/9409?modelVersionId=90854'),
     ('AOM3'                                  , 'aom3'            , 'anime', 'AbyssOrangeMix3'        , 'https://civitai.com/models/9942?modelVersionId=11814'),
     ('AOM3A1'                                , 'aom3a1'          , 'anime', 'AbyssOrangeMix3 A1'     , 'https://civitai.com/models/9942?modelVersionId=11813'),
     ('AOM3A1B'                               , 'aom3a1b'         , 'anime', 'AbyssOrangeMix3 A1B'    , 'https://civitai.com/models/9942?modelVersionId=17233'),
@@ -119,9 +138,11 @@ extra_prompt_mapping = {
 }
 sfw_options = [True, False]
 sfw_extra_neg_prompt = ", nsfw, naked, nude"
-
-results = []
 default_neg_prompt = get_neg_prompt()
+
+checkpoint_map = {}
+prompt_map = {}
+category_map = { "anime": [], "other": [], "photo": [] }
 
 for checkpoint_data, prompt_data, sfw in product(checkpoints, prompts, sfw_options):
     checkpoint, checkpoint_id, category, checkpoint_name, url = checkpoint_data
@@ -152,29 +173,36 @@ for checkpoint_data, prompt_data, sfw in product(checkpoints, prompts, sfw_optio
     print(expected_path)
 
     if not os.path.exists(expected_path):
-        queue_prompt(json_prompt)
+        imgs_generated = 0
+        set_batch_size(imgs_per_batch)
+        while imgs_generated < imgs_per_prompt:
+            set_seed(starting_seed + imgs_generated)
+            queue_prompt(json_prompt)
+            imgs_generated += imgs_per_batch
         while not os.path.exists(expected_path):
             time.sleep(0.1)
+        print("")
     else:
         print("exists, skipping")
 
-    result_entry = {
-        'checkpoint_id': checkpoint_id,
+    checkpoint_map[checkpoint_id] = { 
         'checkpoint': checkpoint,
         'checkpoint_name': checkpoint_name,
         'category': category,
-        'prompt': prompt,
-        'prompt_id': prompt_id,
-        'sfw': sfw,
-        'output_prefix': output_prefix,
     }
-    results.append(result_entry)
+    prompt_map[prompt_id] = prompt
+    if checkpoint_id not in category_map[category]:
+        category_map[category].append(checkpoint_id)
 
     print("-----")
 
-# store the results in a JSON file
-with open('results.json', 'w') as file:
-    json.dump(results, file)
+# store the results in JSON files
+with open('checkpoint_map.json', 'w') as file:
+    json.dump(checkpoint_map, file, indent=2)
+with open('prompt_map.json', 'w') as file:
+    json.dump(prompt_map, file, indent=2)
+with open('category_map.json', 'w') as file:
+    json.dump(category_map, file, indent=2)
 
 repo_dir = os.getcwd()
 
